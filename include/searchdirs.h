92e4b779318b1dfdf9c4a91ebf2171c992320926
rkiesling <rk3314042@gmail.com>
2019-11-19 21:34:20 -0700

    ctalkmethods.1 updates

--- include/searchdirs.h
+++ include/searchdirs.h
@@ -1,12 +1,27 @@
 /* This is a machine generated file.  Do not edit! */
 
 char *searchdirs[] = {
-"/usr/lib/gcc/i686-linux-gnu/5/include",
-"/usr/local/include",
-"/usr/lib/gcc/i686-linux-gnu/5/include-fixed",
-"/usr/include/i386-linux-gnu",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.1.0",
 "/usr/include",
+"/Users/rkiesling/builds/ctalk",
+"/usr/local/include",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.1.0/include",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include",
+"/usr/include",
+"/System/Library/Frameworks",
+"/Library/Frameworks",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.1.0",
+"/usr/include",
+"/Users/rkiesling/builds/ctalk",
+"/usr/local/include",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.1.0/include",
+"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include",
+"/usr/include",
+"/System/Library/Frameworks",
+"/Library/Frameworks",
+"/usr/X11R6/include",
 (void *)0
 };
 #define __have_preloaded_searchdirs
-#define N_PATHS 6
+#define N_PATHS 20
