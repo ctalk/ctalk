7a34cb830d99fcfa0902d5b9f59393c222e3a29a
rkiesling <rk3314042@gmail.com>
2020-12-21 11:18:14 -0700

    updated build to check for the existence of Ctalk cache directory

--- build/set_cache_perms.sh
+++ build/set_cache_perms.sh
@@ -3,6 +3,9 @@
 INSTUSER=`cat .confname`
 INSTGROUP=`cat .confgroup`
 INSTUSERHOMEDIR=`cat .confuserhomedir`
+CTALKDIR=`cat .confuserhomedir`/.ctalk
 
-cd $INSTUSERHOMEDIR && chown -R $INSTUSER .ctalk
-cd $INSTUSERHOMEDIR && chgrp -R $INSTGROUP .ctalk
+if [ -d $CTALKDIR ]; then
+    cd $INSTUSERHOMEDIR && chown -R $INSTUSER .ctalk
+    cd $INSTUSERHOMEDIR && chgrp -R $INSTGROUP .ctalk
+fi
