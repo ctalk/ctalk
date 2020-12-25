8807fc7dc6ff40a9f4395362162f538e54ad1554
rkiesling <rk3314042@gmail.com>
2020-12-25 07:15:26 -0700

    Updated the build configuration for RGBAColor class and lib/xmenu.c.

---
+++ lib/xmenu.c
@@ -0,0 +1,40 @@
+/* $Id: xmenu.c,v 1.1 2020/12/25 14:02:39 rkiesling Exp $ -*-c-*-*/
+
+/*
+  This file is part of Ctalk.
+  Copyright © 2014, 2018 - 2020  Robert Kiesling, rk3314042@gmail.com.
+  Permission is granted to copy this software provided that this copyright
+  notice is included in all source code modules.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation, 
+  Inc., 51 Franklin St., Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include "object.h"
+#include "message.h"
+#include "ctalk.h"
+
+#if ! defined (DJGPP) && ! defined (WITHOUT_X11)
+#include "x11defs.h"
+#include <X11/Xutil.h>
+
+extern Display *display;   /* Defined in x11lib.c. */
+
+#endif /* ! defined (DJGPP) && ! defined (WITHOUT_X11) */
